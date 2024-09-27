bff55b421cf27ec2513f640eab98404d6cb26b87
staneby <stanislaunebyshyniets@gmail.com>
2024-09-27 14:25:46 +0300

    add UBoardNavigationSystem

---
+++ Source/KP_Minigame/Public/BoardNavigationSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BoardNavigationSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KP_MINIGAME_API UBoardNavigationSystem : public UObject
+{
+	GENERATED_BODY()
+	
+};
