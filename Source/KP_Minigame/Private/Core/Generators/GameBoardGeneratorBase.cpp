7f17396fed3c16a6526945d5bb444368d7ded08d
miha15z <mihalo15z.work@gmail.com>
2024-09-30 09:18:14 +0300

    test generator

--- Source/KP_Minigame/Private/Core/Generators/GameBoardGeneratorBase.cpp
+++ Source/KP_Minigame/Private/Core/Generators/GameBoardGeneratorBase.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Core/Generators/GameBoardGeneratorBase.h"
+#include "Core/Generators/Data/GenDataAsset.h"
 
+FBoardData UGameBoardGeneratorBase::GenerateGameBoard_Implementation(UGenDataAsset* GenData, UObject* WorldContext) const
+{
+	check(WorldContext);
+	auto World = WorldContext->GetWorld();
+	check(World && GenData);
+	auto CellClass = GenData->GetCellClass();
+	auto BoardSize = GenData->GetBoardSize();
+	auto LeftOffset = GenData->GetCellLeftOffset();
+	auto RightOffset = LeftOffset * FVector(1.f, -1.f, 1.f);
+	auto TopCellLocation = GenData->GetTopCellLocation();
+
+	return FBoardData();
+}
--- Source/KP_Minigame/Public/Core/Generators/GameBoardGeneratorBase.h
+++ Source/KP_Minigame/Public/Core/Generators/GameBoardGeneratorBase.h
@@ -4,14 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Core/KP_Structs.h"
 #include "GameBoardGeneratorBase.generated.h"
+
+
+class UGenDataAsset;
+class ACellActorBase;
+class UWorld;
+
+
 
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType, Blueprintable)
 class KP_MINIGAME_API UGameBoardGeneratorBase : public UObject
 {
 	GENERATED_BODY()
 	
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = Generation)
+	FBoardData GenerateGameBoard(UGenDataAsset* GenData, UObject* WorldContext) const;
+	virtual FBoardData GenerateGameBoard_Implementation(UGenDataAsset* GenData, UObject* WorldContext) const;
 };
