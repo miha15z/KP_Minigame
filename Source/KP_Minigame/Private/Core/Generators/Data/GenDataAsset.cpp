2373db9f225dd071d729541d39fd70e6cf1563e8
miha15z <mihalo15z.work@gmail.com>
2024-09-30 09:07:36 +0300

    GameBoardDA

--- Source/KP_Minigame/Private/Core/Generators/Data/GenDataAsset.cpp
+++ Source/KP_Minigame/Private/Core/Generators/Data/GenDataAsset.cpp
@@ -3,3 +3,10 @@
 
 #include "Core/Generators/Data/GenDataAsset.h"
 
+
+
+const FKPPlayerData& UGenDataAsset::GetPlayerData(const int32 PlayerId) const
+{
+	check(PlayerId < PlayersData.Num() && PlayerId >= 0);
+	return PlayersData[PlayerId];
+}
--- Source/KP_Minigame/Public/Core/Generators/Data/GenDataAsset.h
+++ Source/KP_Minigame/Public/Core/Generators/Data/GenDataAsset.h
@@ -4,8 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "Core/KP_Structs.h"
 #include "GenDataAsset.generated.h"
-
 
 
 class ACellActorBase;
@@ -25,5 +25,30 @@ protected:
 	TSubclassOf<ACellActorBase> CellClass;
 
 	UPROPERTY(EditDefaultsOnly, Category = GameBoard)
-	TArray<int32> CellModifecation;
+	FVector TopCellLocation;
+
+	//  RightOffset = (LeftOffset.X, -LeftOffset.Y, LeftOffset.Z)
+	UPROPERTY(EditDefaultsOnly, Category = GameBoard)
+	FVector LeftOffset;
+
+	// PlayersData[0] = player data, other = AI
+	UPROPERTY(EditDefaultsOnly, Category = "GameBoard | Players")
+	TArray<FKPPlayerData> PlayersData;  
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	const FKPPlayerData& GetPlayerData(const int32 PlayerId) const;
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetBoardSize() const {return BoardSize;}
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE TSubclassOf<ACellActorBase> GetCellClass() const { return CellClass; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE FVector GetTopCellLocation() const { return TopCellLocation; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE FVector GetCellLeftOffset() const { return LeftOffset; }
 };
