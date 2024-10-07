3cf28caaad2af653e8af048151619130a15a3121
staneby <stanislaunebyshyniets@gmail.com>
2024-10-07 14:40:36 +0300

    add basic fate stone classes

---
+++ Source/KP_Minigame/Public/GameplayAbilityFateStone.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilityFateStone.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KP_MINIGAME_API UGameplayAbilityFateStone : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/KP_Minigame/Public/GameplayEffectFateStone.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectFateStone.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KP_MINIGAME_API UGameplayEffectFateStone : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
