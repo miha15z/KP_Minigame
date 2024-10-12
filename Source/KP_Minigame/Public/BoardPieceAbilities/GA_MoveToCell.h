6fec6864dc8dd3bb49eaf272bae97d6aa69cf0d1
miha15z <mihalo15z.work@gmail.com>
2024-10-12 16:29:03 +0300

    make GA Move To Cell

---
+++ Source/KP_Minigame/Public/BoardPieceAbilities/GA_MoveToCell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_MoveToCell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KP_MINIGAME_API UGA_MoveToCell : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
