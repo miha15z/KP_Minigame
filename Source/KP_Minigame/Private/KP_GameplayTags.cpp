b6b27a55fd6221098622bc76b991c157677a9a3f
miha15z <mihalo15z.work@gmail.com>
2024-10-13 17:17:16 +0300

    add tags

--- Source/KP_Minigame/Private/KP_GameplayTags.cpp
+++ Source/KP_Minigame/Private/KP_GameplayTags.cpp
@@ -29,9 +29,32 @@ FGameplayTag KP_GameplayTags::FindTagByString(const FString& TagString, bool bMa
 
 namespace KP_GameplayTags
 {
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateWin, "WinCondition", "Gloabls Win Conditions");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateWin_KillKing, "WinCondition.KillKing", "Win : Kill King");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateWin_ReachThePoint, "WinCondition.ReachThePoint", "Win : Reach the point ");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_IsDead, "Ability.ActivateFail.IsDead", "Ability failed to activate because its owner is dead.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_Cooldown, "Ability.ActivateFail.Cooldown", "Ability failed to activate because it is on cool down.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_Cost, "Ability.ActivateFail.Cost", "Ability failed to activate because it did not pass the cost checks.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_TagsBlocked, "Ability.ActivateFail.TagsBlocked", "Ability failed to activate because tags are blocking it.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_TagsMissing, "Ability.ActivateFail.TagsMissing", "Ability failed to activate because tags are missing.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_Networking, "Ability.ActivateFail.Networking", "Ability failed to activate because it did not pass the network checks.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ActivateFail_ActivationGroup, "Ability.ActivateFail.ActivationGroup", "Ability failed to activate because of its activation group.");
 
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_ShowWin, "WonPlayer", "Won player");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_ActivateWin, "GameplayEvent.WinCondition", "Gloabls Win Conditions");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_ActivateWin_KillKing, "GameplayEvent.WinCondition.KillKing", "Win : Kill King");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_ActivateWin_ReachThePoint, "GameplayEvent.WinCondition.ReachThePoint", "Win : Reach the point ");
+
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameState_ShowGameResult, "WonPlayer", "Won player");
+
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_MoveBoardPiece, "GameplayEvent.BoardPiece.MoveToCell", "Move event");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_KillBoardPiece, "GameplayEvent.BoardPiece.KellBoardPiece", "Kill board piece");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_UpgradePawn, "GameplayEvent.BoardPiece.UpgradePawn", "Upgrade pawn");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayEvent_ApplyAbilitiesToBoardPiece, "GameplayEvent.Cell.ApplyToBoardPiece", "Apply to board piece");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_Move, "BoardPieceState.Move", "Move");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_None, "BoardPieceState.None", "None");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_Selectable, "BoardPieceState.Selectable", "Board piece is selectable");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_Selected, "BoardPieceState.Selected", "Board piece selected");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_StartDead, "BoardPieceState.StartDead", "Start Dead");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_EndDead, "BoardPieceState.EndDead", "End Dead");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(BoardPieceState_Dead, "Dead", "Dead");
+
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayCue_BoardPiece_Move, "GameplayCue.MoveBoardPiece", "MoveBoardPieceCue")
 }
