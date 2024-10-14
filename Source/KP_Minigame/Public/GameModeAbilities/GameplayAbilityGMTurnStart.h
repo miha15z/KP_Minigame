63f2af1cd7be2d16a8d7b427f851996fd47f4b27
miha15z <mihalo15z.work@gmail.com>
2024-10-14 11:59:50 +0300

    Add GM Abilities

---
+++ Source/KP_Minigame/Public/GameModeAbilities/GameplayAbilityGMTurnStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilityGMTurnStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KP_MINIGAME_API UGameplayAbilityGMTurnStart : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
